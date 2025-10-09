04bdd02144902d0dba2ddc3b7909b09ec0974ccb
Mohammed Eweedan <mohammedawidan@yahoo.com>
2025-10-09 16:21:13 +0200

    dasdasdasd

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,7 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import serverless from "serverless-http";
 import app from "../src/index";
-
 const handler = serverless(app);
 export default async function (req: VercelRequest, res: VercelResponse) {
   return handler(req as any, res as any);
